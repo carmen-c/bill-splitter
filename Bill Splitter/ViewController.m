6611a4a8575e95821089f1cf7c020e42fef84842
carmen-c <carmen.csmwc@gmail.com>
2016-11-12 16:08:07 -0800

    commit

--- Bill Splitter/ViewController.m
+++ Bill Splitter/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *totalAmount;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *splitAmount;
+@property (weak, nonatomic) IBOutlet UILabel *numberOfPeople;
 @end
 
 @implementation ViewController
@@ -18,12 +21,30 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)slider:(id)sender {
+    int rounded = roundf(self.slider.value);
+    self.numberOfPeople.text = [NSString stringWithFormat:@"Between %d people",rounded];
 }
+
+- (IBAction)calculateSplitAmount:(id)sender {
+    
+    if (self.totalAmount.text && self.totalAmount.text.length > 0) {
+        int rounded = roundf(self.slider.value);
+        NSDecimalNumber *numberOfPeople =[[NSDecimalNumber alloc] initWithInt:rounded];
+        NSDecimalNumber *totalAmount = [[NSDecimalNumber alloc]initWithString:self.totalAmount.text];
+        NSDecimalNumber *split = [totalAmount decimalNumberByDividingBy:numberOfPeople];
+        
+        NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc]init];
+        [numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+        [numberFormatter setMaximumFractionDigits:2];
+        
+        self.splitAmount.text = [NSString stringWithFormat:@"$ %@", [numberFormatter stringFromNumber:split]];
+
+    }else {
+        self.splitAmount.text = [NSString stringWithFormat:@"no bill was entered"];
+    }
+}
+
 
 
 @end
